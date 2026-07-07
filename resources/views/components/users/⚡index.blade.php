ce4d1954c36dc91ca6b5c2db57de47ca7d8e3fe2
Glen <glendonnalpasan1@gmail.com>
2026-07-07 10:58:04 +0800

    Add user-related columns to users table and update sidebar navigation

---
+++ resources/views/components/users/⚡index.blade.php
@@ -0,0 +1,21 @@
+<?php
+
+use Livewire\Component;
+
+
+new class extends Component
+{
+    public function render(){
+
+        return view('components.users.⚡index', [
+            
+        ])->title('Users');
+
+    }
+};
+
+?>
+
+<div>
+    {{-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama --}}
+</div>
